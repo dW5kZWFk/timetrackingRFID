036e1521ae3c55d686925665a8921b74a11d4b65
s0ulr34v3r <an-dembowski@t-online.de>
2022-11-26 10:33:22 +0100

    modified schema again (auto increment primary key!)

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
-CREATE TABLE employee_state (uid int primary key, start_time [timestamp], name text, state text);
-CREATE TABLE working_time (id int, employee_uid int, date text, time text, FOREIGN KEY(employee_uid) REFERENCES employee_state(uid));
+CREATE TABLE employee_state (uid integer primary key not null, start_time [timestamp], name text not null, state text not null);
+CREATE TABLE working_time (id integer primary key not null key autoincrement, employee_uid integer not null, date text not null, time text not null, FOREIGN KEY(employee_uid) REFERENCES employee_state(uid));
 
+55396084254
